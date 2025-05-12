768455e0dd7420dcb44997f5fcec0a5ba5e227f3
prathyusha1993 <prathyusha9310@gmail.com>
2025-05-12 16:46:02 -0700

    recat practise 2025

---
+++ src/components/useWindowWidth.jsx
@@ -0,0 +1,18 @@
+function useWindowWidth() {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener('resize', handleResize);
+    
+      return () => {
+        window.removeEventListener('resize',handleResize);
+      }
+    }, [])
+    return width;
+}
+
+function App(){
+    const width = useWindowWidth();
+    return <p>Window Width: {width}</p>
+}
